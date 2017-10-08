4ef38f27a2e0daa98df1930b926f6d7ba0d17711
shirinR <shirin.raad@gmail.com>
2017-10-08 14:02:10 -0700

    edit .py file

--- get_final_values/get_final_value.py
+++ get_final_values/get_final_value.py
@@ -8,7 +8,12 @@ A6 = [[i,i*i] for i in A1]
 
 # NOTE Answers:
 # zip(('a','b','c','d','e'),(1,2,3,4,5)) => [('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)]
+# NOTE: result is a list of tuple and as you know Tuples are immutable.
+# As you know lists are mutable so order may vary each time.
+
 # A0 = dict(zip(('a','b','c','d','e'),(1,2,3,4,5))) => {'a':1,'b':2,'c':3,'d':4,'e':5}
+# NOTE: result is a dictionary.
+
 # A1 = range(10) => [0,1,2,3,4,5,6,7,8,9]
 # A2 = sorted([i for i in A1 if i in A0]) => [1,2,3,4,5]
 # A3 = sorted([A0[s] for s in A0]) => [1,2,3,4,5]
