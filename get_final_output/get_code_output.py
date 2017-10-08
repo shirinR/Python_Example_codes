3c27d3f184970a659ddc8d9496e13dcf5bcd79cb
shirinR <shirin.raad@gmail.com>
2017-10-08 15:08:42 -0700

    edit typo

--- get_final_output/get_code_output.py
+++ get_final_output/get_code_output.py
@@ -3,8 +3,8 @@ def f(x,l=[]):
         l.append(i*i)
     print(l)
 
-# f(2) => [0,1] here it refers to new clock of memory
-# f(3,[3,2,1]) =>[3,2,1,0,1,4] here it refers to new clock of memory
+# f(2) => [0,1] here it refers to new block of memory
+# f(3,[3,2,1]) =>[3,2,1,0,1,4] here it refers to new block of memory
 # f(3) => [0,1,0,1,4] because it referes to the same block of memory as the first one
 
 # NOTE: another way of coding above code
@@ -26,4 +26,4 @@ def f(x,l=[]):
 # for i in range(3):
 #     l.append(i*i)
 #
-# print(l)            # [0, 1, 0, 1, 4] 
+# print(l)            # [0, 1, 0, 1, 4]
