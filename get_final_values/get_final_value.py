dc0e69ac7447ea42ddea60d130a51ed014bd7556
shirinR <shirin.raad@gmail.com>
2017-10-08 14:04:07 -0700

    edit .py file

--- get_final_values/get_final_value.py
+++ get_final_values/get_final_value.py
@@ -15,7 +15,7 @@ A6 = [[i,i*i] for i in A1]
 # NOTE: result is a dictionary.
 
 # A1 = range(10) => [0,1,2,3,4,5,6,7,8,9]
-# A2 = sorted([i for i in A1 if i in A0]) => [1,2,3,4,5]
+# A2 = sorted([i for i in A1 if i in A0]) => []
 # A3 = sorted([A0[s] for s in A0]) => [1,2,3,4,5]
 # A4 = [i for i in A1 if i in A3] => [1,2,3,4,5]
 # A5 = {i:i*i for i in A1} => {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
